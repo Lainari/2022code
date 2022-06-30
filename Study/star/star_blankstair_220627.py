b3f1862b2960b6b993ef923a268297a223da27be
Lainari <skskhy1021@g.yju.ac.kr>
2022-06-30 10:03:29 +0900

    Delete star_blankstair_220627.py

--- Study/star/star_blankstair_220627.py
+++
@@ -1,9 +0,0 @@
-# 220627수업
-row = 5  # 세로줄 확인
-
-for value_row in range(row):  # row번 반복 <- 세로
-    for blank_count in range((row-1)-value_row):  # 공백을 출력하는 for문
-        print(" ", end="")
-    for value_col in range(3+value_row):  # 공백이 끝나고 *를 출력하는 for문
-        print("*", end="")
-    print()  # 가로줄이 끝나면 세로줄 출력
