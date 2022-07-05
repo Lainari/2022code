3f323f816682f9f3825ab9dea7e3a0deeda5e11b
Lainari <108247620+Lainari@users.noreply.github.com>
2022-07-05 12:33:57 +0900

    Create sandwatch_220701.py

---
+++ Study/star(220701~08)/sandwatch_220701.py
@@ -0,0 +1,20 @@
+# 220701 수업
+# 모래시계를 만들어봅시다
+
+# 0. 층 만들기
+row = 13
+
+# 1. 세로줄 만들기
+for star_row in range(row):
+    # 2. 가로줄 만들기
+    for star_col in range(row):
+        # 5. 세로줄의 첫번째와 마지막은 전체출력하고
+        # 세로줄과 가로줄의 값이 같아지거나 가로줄의 값이 층-세로줄-1 값과 같아질때 별을 출력하고
+        # 그 외에는 공백을 출력하라
+        if star_row == 0 or star_row == row-1 or star_col == star_row or row-star_row-1 == star_col:
+            # 3. 별 출력, 줄바꿈 X
+            print("*", end="")
+        else:
+            print(" ", end="")
+    # 4. 가로줄 종료 후 줄바꿈
+    print()
