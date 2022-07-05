ca90b7d260797cbe41e2f733c3f04458e235f82a
Lainari <108247620+Lainari@users.noreply.github.com>
2022-07-05 17:45:50 +0900

    Update star_left_triangle3_220705.py

--- Study/star(220701~08)/star_left_triangle3_220705.py
+++ Study/star(220701~08)/star_left_triangle3_220705.py
@@ -1,15 +1,19 @@
 # 좌측에서 나오는 삼각형 만들기
 
-# 0. 층 생성
-row = 9
+# 0. 층 만들기
+row = 7
 
-# 1. 세로 층 생성
+# 1. 세로층 제작
 for star_row in range(row):
-    # 2. 조건에 따라 달라지는 가로줄 출력
-    if star_row < (row // 2 + 1) :
-        for star_col in range(star_row+1):
+    # 2. 조건에 따라 변하는 가로줄
+    # 조건 : 중간값(row//2+1)을 넘었나 안남었나?
+    if star_row < (row//2)+1: # 중간값 이전
+        # 3. 가로줄 제작
+        for star_col in range(star_row+1): # 1씩 상승구조
             print("*",end="")
-    else:
-        for star_col in range(row-star_row):
+        print()
+    else: # 중간값 이후
+        # 4. 가로줄 제작
+        for star_col in range(row-star_row): # 1씩 감소구조(기준 = 중간값)
             print("*",end="")
-    print()
+        print()
