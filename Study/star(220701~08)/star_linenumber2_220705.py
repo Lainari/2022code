ef76212274d5a6fc54575b1f8d6192eba0e34b6e
Lainari <108247620+Lainari@users.noreply.github.com>
2022-07-05 12:33:44 +0900

    Create star_linenumber2_220705.py

---
+++ Study/star(220701~08)/star_linenumber2_220705.py
@@ -0,0 +1,19 @@
+# Q4 키보드로부터 값을 입력받아 입력받은 값 만큼 층이 나오고 중간값을 기준으로 하행하는 * 패턴을 만들어라
+
+# 0. 층 입력
+row = 5
+
+# 1. 세로층 제작
+for star_row in range(5):
+    # 2. 중간값 판별
+    if star_row < row//2+1:
+        # 3. 세로층의 중간값에 따른 패턴 1
+        for star_col in range(star_row+1):
+            print("*",end="")
+        print()
+    else:
+        # 4. 세로층의 중간값에 따른 패턴 2
+        for star_col in range(row-star_row):
+            print("*",end="")
+        print()
+        
