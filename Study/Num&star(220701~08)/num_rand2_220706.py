dbffe10852cde790f93f603509b19b350596a08a
Lainari <108247620+Lainari@users.noreply.github.com>
2022-07-06 12:27:47 +0900

    Update num_rand2_220706.py

--- Study/Num&star(220701~08)/num_rand2_220706.py
+++ Study/Num&star(220701~08)/num_rand2_220706.py
@@ -1,14 +1,13 @@
-# 랜덤함수를 사용하기위해 랜덤함수 모듈 import <- 필수
 import random
 
-# 0 ~ N 실수 랜덤값
+# 0 ~ 5 정수 랜덤값
 for value in range(10):
-    num = random.random()*10 # 랜덤함수인 random.random() 과 0 ~ N 까지 범위를 정해주는 *N
+    num = int(random.random()*5)
     print(num)
-print()
-print("------------------------")
-print()
-# 0 ~ N 정수 랜덤값
-for value in range(10):
-    num = int(random.random()*10)
+
+print("----------------------------")
+
+# -5 ~ 10 정수 랜덤값
+for value in range(50):
+    num = int(random.random()*11)-5
     print(num)
