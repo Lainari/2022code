3028ccc405aa3993b09099818157f7278473b52f
Lainari <108247620+Lainari@users.noreply.github.com>
2022-07-07 11:16:46 +0900

    Update num_random1_220707.py

--- Study/Num&star(220701~08)/num_random1_220707.py
+++ Study/Num&star(220701~08)/num_random1_220707.py
@@ -1,9 +1,23 @@
 # 랜덤 함수
 # -> 난수 발생기
 
-import random
+# import random
 
-# 0이상 1미만의 실수를 반환
-# 0.0 ~ 0.9999999999......
-for value in range(10):
-    print(int(random.random()* 3)+1)
+# # 0이상 1미만의 실수를 반환
+# # 0.0 ~ 0.9999999999......
+# for value in range(10):
+#     print(int(random.random()* 3)+1)
+
+
+# List : 자료 구조(Data Structure)
+# Composite Data Type : -> Array 와 Linked List
+# Abstract Data Type : -> List
+# 학생 100 명의 성적을 입력 받아 평균을 계산하시오.
+
+student_1 = 10
+student_2 = 20
+student_3 = 5
+student_4 = 60
+student_5 = 100
+
+
