8dd1845fb322552e52fab58de6efc5d899bb846c
Lainari <108247620+Lainari@users.noreply.github.com>
2022-07-11 20:06:13 +0900

    Update num_random_baseball_220711.py

--- Study/Num&star(220711~15)/num_random_baseball_220711.py
+++ Study/Num&star(220711~15)/num_random_baseball_220711.py
@@ -1,10 +1,13 @@
 # • 게임 시작 시 0~9사이 정수 중 중복 값이 없는 난수 3개 생성
 import random
+
 gameList=[]
+
 while len(gameList) < 3:
     check = random.randint(0,9)
     if check not in gameList:
         gameList.append(check)
+
 # • 키보드로부터 0~9사이 정수 3개를 입력 받고 결과 값을 출력 (예외처리 X)
 playCount = 1
 strikeOut = 0
@@ -12,6 +15,7 @@ while playCount < 5:
     print("시도횟수 : ",playCount)
     playerList=[]
     print("정수 3개를 입력하세용~~^_^")
+    
     while len(playerList) < 3:
         playerList.append(int(input()))
     print()
@@ -31,12 +35,11 @@ while playCount < 5:
     # 판별이 끝난 후 해당 값에 따른 출력문구
     if strike == 0 and ball == 0:
         strikeOut += 1
+        print("Out : 아웃 1번")
     if strike > 0:
         print(strike," strike",end=" ")
     if ball > 0:
-        print(ball," ball",end="")
-    if strikeOut > 0:
-        print("Out : 아웃 1번")
+        print(ball," ball",end=" ")
     playCount += 1
     print()
 # • 아래 경우 게임 Lose – 시도 횟수 >= 5 or – Strike out == 2
