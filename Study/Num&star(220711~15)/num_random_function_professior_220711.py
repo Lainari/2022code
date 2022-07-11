2a9307fc9beaa6b670f9ab66f2c6df872c25ed5d
Lainari <108247620+Lainari@users.noreply.github.com>
2022-07-11 18:35:59 +0900

    Update num_random_function_professior_220711.py

--- Study/Num&star(220711~15)/num_random_function_professior_220711.py
+++ Study/Num&star(220711~15)/num_random_function_professior_220711.py
@@ -23,12 +23,46 @@ for value in myList:
     # 최대 - value > max :  value <-max
     if value > max:
         max = value
+
     # 합계
     sum = sum + value
+
 #  평균
 avg = sum / len(myList)
 
 print(min,max,sum,avg)
-#  중복값, 중복 회수
 
+temp=[]
+duplicatedNum = [0] * len(myList) # 같은 원소를 len(myList) 개수만큼 생성
+
+#  중복값, 중복 회수
+for value in myList:
+    # value in temp -> 중복값
+    # duplicatedNum[value - 1] 1 증가
+    if value in temp:
+        duplicatedNum[value - 1] += 1
+    else:
+    # else 신규값
+    # temp에 신규 값 추가
+        temp.append(value)
+index = 1
+for value in duplicatedNum:
+    if value >= 1:
+        print(index, value + 1)
+
+    index += 1
 #  구간 별 히스토그램
+numForInterval = [0]*4
+
+for value in myList:
+    if 1 <= value <= 5:
+        numForInterval[0] += 1
+    elif 6 <= value <= 10:
+        numForInterval[1] += 1
+    elif 11 <= value <= 15:
+        numForInterval[2] += 1
+    elif 16 <= value <= 20:
+        numForInterval[3] += 1
+
+for value in numForInterval:
+    print("*"*value)
