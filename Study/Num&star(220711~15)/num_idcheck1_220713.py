9dbd7274edc44787bf8dbbfc78619fef3f58d1ac
Lainari <108247620+Lainari@users.noreply.github.com>
2022-07-13 19:23:09 +0900

    Update num_idcheck1_220713.py

---
+++ Study/Num&star(220711~15)/num_idcheck1_220713.py
@@ -0,0 +1,21 @@
+# 유효 주민등록번호 검사 알고리즘
+
+# 체크수는 고정되어있음(상수 : 234567892345), 마지막 자리가 비어있음
+checkNum = "234567892345"
+# 주민등록 번호 입력 (변수 : 990000-1111111)
+userIdList = input("주민번호 13자리를 입력하세요 : ")
+# 주민등록번호 값과 체크수 값과 곱하기, 덧셈 계산
+count = 0
+sum = 0
+for value in userIdList:
+    if value.isdigit():
+        sum += (int(value)*int(checkNum[count]))
+        count += 1
+        if count == 12:
+            break
+# 체크 계산 => 11 - (덧셈 계산을 11로 나눈 나머지) = 마지막 자리값
+check = 11 - (sum % 11)
+if check == userIdList[-1] or check % 10 == userIdList[-1]:
+    print("유효한 주민번호 입니다.")
+else:
+    print("유요하지 않은 주민번호입니다.")
