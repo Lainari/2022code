57eddac94a7e30068acc35900ca0dce9ad84532c
Lainari <108247620+Lainari@users.noreply.github.com>
2022-07-14 18:23:55 +0900

    Update num_random_baseball11_220713.py

--- Study/Num&star(220711~15)/num_random_baseball11_220713.py
+++ Study/Num&star(220711~15)/num_random_baseball11_220713.py
@@ -1,58 +1,58 @@
-def play(a,b,c):
-# 승리 : strike >= 3
-    if a == 3:
-        print("삼진")
-# 패배 1 : strikeOut >= 2
-    if b == 2:
-        print("아웃 2번 끝")
-# 패배 2 : playCount >= 5
-    if c == 5:
-        print("게임 횟수 초과")
-
-# 0 ~ 9 까지 난수 생성, 중복값 X
+# • 게임 시작 시 0~9사이 정수 중 중복 값이 없는 난수 3개 생성
 import random
 
-gameList = []
+gameList=[]
+
 while len(gameList) < 3:
     check = random.randint(0,9)
     if check not in gameList:
         gameList.append(check)
-
-playCount = 1
+        
+playCount = 0
 strikeOut = 0
-# 사용자에게 키보드로부터 3가지의 값을 입력 받기
+
 while True:
-    print(gameList)
-    print("시도 횟수 : ",playCount)
+    playCount += 1
+# • 아래 경우 게임 Lose – 시도 횟수 >= 5
+    if playCount >= 5:
+        print("게임횟수 초과")
+        print("아까비~~~졌네용..")
+        print("정답은 : ",gameList,"입니다")
+        break
+# • 키보드로부터 0~9사이 정수 3개를 입력 받고 결과 값을 출력 (예외처리 X)
+    print("시도횟수 : ",playCount)
+    print("정수 3개를 입력해주세용~~^_^")
     playerList=[]
-    for value in range(3):
-        playerList.append(int(input("정수를 입력해주세요 : ")))
-    a=0
+    while len(playerList) < 3:
+        playerList.append(int(input()))
+# 스트라이크, 볼, 아웃 판별식
     strike = 0
     ball = 0
-# 스트라이크, 볼 판정
     for value in range(3):
         if playerList[value] in gameList:
             if playerList[value] == gameList[value]:
                 strike += 1
             else:
                 ball += 1
-    if strike == 0 and ball == 0:
-        strikeOut += 1
-    else:
-        print(strike," Strike",ball," Ball")
-    playCount += 1
-    if strike >= 3 or strikeOut >=2 or playCount>=5:
+# • 아래 경우 게임 Win– 컴퓨터에서 생성 한 난수 값을 자리 순서대로 맞출 경우
+    if strike == 3:
+        print("삼진~~!! 축하드립니다!")
+        print("정답은 : ",gameList,"입니다")
+        print("입력값 : ",playerList)
         break
-play(strike,strikeOut,playCount)
-    
-# # 승리 : strike >= 3
-#     if strike >= 3:
-#         break
-# # 패배 1 : strikeOut >= 2
-#     if strikeOut >= 2:
-#         break
-# # 패배 2 : playCount >= 5
-#     playCount += 1
-#     if playCount >= 5:
-#         break
+    else:
+        if strike == 0 and ball == 0:
+            strikeOut += 1
+            if strikeOut == 1:
+                print("Out : 아웃 1번")
+                
+            else: # Strike out == 2 될 경우 종료
+                print("2 아웃!! ㅠㅠ")
+                print("아까비~~~졌네용..")
+                print("정답은 : ",gameList,"입니다")
+                break
+        if strike > 0:
+            print(strike," Strike",end=" ")
+        if ball > 0:
+            print(ball," Ball",end=" ")
+    print()
