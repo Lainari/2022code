9251235e046f741ac4a5f004226b6982b6b17ecf
Lainari <108247620+Lainari@users.noreply.github.com>
2022-07-14 18:23:50 +0900

    Update num_random_baseball6_220712.py

--- Study/Num&star(220711~15)/num_random_baseball6_220712.py
+++ Study/Num&star(220711~15)/num_random_baseball6_220712.py
@@ -1,80 +1,74 @@
 # • 게임 시작 시 0~9사이 정수 중 중복 값이 없는 난수 3개 생성
 import random
 
-# 난수 리스트 생성
-gameList = []
-
-# 난수 3개 생성할 동안 중복값 판별
+gameList =[]
 while len(gameList) < 3:
+    # 난수 생성
     check = random.randint(0,9)
+    # 난수 생성 값이 게임 리스트에 없을 경우 추가, 아니면 재생성
     if check not in gameList:
         gameList.append(check)
 
-# • 키보드로부터 0~9사이 정수 3개를 입력 받고 결과 값을 출력 (예외처리 X)
-
-# 게임이 특정 조건을 만족하기 전에는 계속 실행
+# 변수 생성
 strikeOut = 0
-playCount = 0
+playCount = 1
 
-while True:
-
-    # 게임횟수를 증가 시키고 조건 값보다 높으면 게임은 종료된다
-    playCount += 1
-    if playCount >= 5:
-        print("게임횟수 초과")
-        print("아까비~~~졌네용..")
-        print("정답은 : ",gameList,"입니다")
-        break
-
-    # 키보드로부터 정수 3개 입력받기
-    playerList=[]
+# • 키보드로부터 0~9사이 정수 3개를 입력 받고 결과 값을 출력 (예외처리 X)
+while True: # 게임은 특정한 상황이 아닌 경우 지속 실행
     print("시도횟수 : ",playCount)
-    print("정수 3개를 입력해주세용~~~^_^")
-    while len(playerList)< 3:
+    print("정수 3개를 입력하세용~~~^_^")
+    playerList=[]
+
+    # 플레이어 리스트 원소가 3개가 될 때까지 입력받기
+    while len(playerList) < 3:
         playerList.append(int(input()))
 
-    # 플레이어와 게임 리스트가 모두 준비되었으므로 볼, 스트라이크, 스트라이크 아웃 판별식 작성
+    # 변수생성
     strike = 0
     ball = 0
 
+    # 플레이어 리스트 원소들이 게임 리스트에 속하는지 판별
     for value in range(3):
-        # 게임리스트 안에 해당 값이 있는지 판별
-        if playerList[value] in gameList: 
-            # 있으면 그 값이 동일한 위치에 있는지 판별
+        if playerList[value] in gameList:
+            # 있다면 스트라이크, 아니라면 볼을 판별
             if playerList[value] == gameList[value]:
                 strike += 1
-            # 아니면 볼
             else:
                 ball += 1
-        # 해당 값이 없으면 다음 순번으로 넘어가기
+        # 리스트에 속하지 않은 원소라면 다음 원소로 넘겨서 그 원소또한 속하는지 판별하게끔 만들기
+        else:
+            continue
 
-    # 스트라이크가 만약 3회라면 게임은 종료된다, 아니면 strike 값과 ball 값을 표출
-    if strike == 3:
-        print("승리~!!! *^o^* ")
-        print("정답   : ",gameList)
-        print("입력값 : ",playerList)
-        break
-
-    # 만일 둘다 0인 경우 (전 원소 불일치) 아웃 카운트 증가
-    elif strike == 0 and ball == 0:
+    # 반복문이 끝나고 strike 와 ball 이 각각 0이라면 Out 이므로 해당 값을 증가시킨다
+    if strike == 0 and ball == 0:
         strikeOut += 1
-
-        # 증가한 Out 카운트가 2 이상이라면 게임은 종료 된다
-        if strikeOut >= 2:
-            print("2스트라이크 아웃입니다! ㅠㅠ")
-            print("아까비~~~졌네용..")
+        # 만일, 증가한 값이 2 라면 break, 아니라면 Out : 아웃 1번을 출력한다
+        if strikeOut == 2:
+            print("스트라이크 아웃 2회! ㅠㅠ")
+            print("아까비~~~졌네용")
             print("정답은 : ",gameList,"입니다")
             break
-
-        # 아닐 경우 Out 카운트가 1번밖에 안났으므로 해당msg 출력
-        print("Out : 아웃 1번")
-
-    # strike 값과 ball 값에 따라 표출하는 msg
+        else:
+            print("Out : 아웃 1번")
+            
+    # 반복문이 끝나고 strike 값이 3회라면 모든 정답을 맞췄으므로 성공 msg와 탈출
     else:
-        if strike > 0:
-            print(strike," Strike",end=" ")
-        if ball > 0:
-            print(ball," Ball",end=" ")
-        print()
+        if strike == 3:
+            print("성공~!!")
+            print("정답은 : ",gameList,"입니다.")
+            break
+        # 아니면 strike 값과 ball 값에 따라 msg 표출
+        else:
+            if strike > 0:
+                print(strike," Strike",end=" ")
+            if ball > 0:
+                print(ball," Ball",end=" ")
+            print()
 
-    # strike, ball, strikeOut 판별식 종료 후 해당 조건이 해당되지 않는다면 다시 게임 초기 상태로 돌아간다
+    # 해당 검사를 마치고 playCount를 증가시키고, 해당 값이 일정 값이 넘으면 break
+    playCount += 1
+    if playCount >= 5:
+        print("게임횟수 초과")
+        print("아까비~~~졌네용")
+        print("정답은 : ",gameList,"입니다")
+        break
