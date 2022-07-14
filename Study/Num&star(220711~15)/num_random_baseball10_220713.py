dbb71155886adaf8b3d86a12c8d87b697b1c93a1
Lainari <108247620+Lainari@users.noreply.github.com>
2022-07-14 18:23:44 +0900

    Update num_random_baseball10_220713.py

--- Study/Num&star(220711~15)/num_random_baseball10_220713.py
+++ Study/Num&star(220711~15)/num_random_baseball10_220713.py
@@ -1,21 +1,26 @@
 # • 게임 시작 시 0~9사이 정수 중 중복 값이 없는 난수 3개 생성
 import random
 
-gameList=[]
+gameList = []
 
-while len(gameList) < 3:
+while len(gameList)<3:
     check = random.randint(0,9)
     if check not in gameList:
         gameList.append(check)
-        
+
 playCount = 0
 strikeOut = 0
 
 while True:
     playCount += 1
-# • 아래 경우 게임 Lose – 시도 횟수 >= 5
+# • 아래 경우 게임 Lose – 시도 횟수 >= 5 – Strike out == 2
     if playCount >= 5:
         print("게임횟수 초과")
+        print("아까비~~~졌네용..")
+        print("정답은 : ",gameList,"입니다")
+        break
+    elif strikeOut == 2:
+        print("2아웃!! ㅠㅠ")
         print("아까비~~~졌네용..")
         print("정답은 : ",gameList,"입니다")
         break
@@ -25,7 +30,7 @@ while True:
     playerList=[]
     while len(playerList) < 3:
         playerList.append(int(input()))
-# 스트라이크, 볼, 아웃 판별식
+# strike, ball, out 판별
     strike = 0
     ball = 0
     for value in range(3):
@@ -34,23 +39,17 @@ while True:
                 strike += 1
             else:
                 ball += 1
-# • 아래 경우 게임 Win– 컴퓨터에서 생성 한 난수 값을 자리 순서대로 맞출 경우
+# • 아래 경우 게임 Win – 컴퓨터에서 생성 한 난수 값을 자리 순서대로 맞출 경우
     if strike == 3:
-        print("삼진~~!! 축하드립니다!")
+        print("삼진~!! 축하합니다")
         print("정답은 : ",gameList,"입니다")
         print("입력값 : ",playerList)
         break
+    elif strike == 0 and ball == 0:
+        strikeOut += 1
+        if strikeOut == 1:
+            print("Out : 아웃 1번")
     else:
-        if strike == 0 and ball == 0:
-            strikeOut += 1
-            if strikeOut == 1:
-                print("Out : 아웃 1번")
-                
-            else: # Strike out == 2 될 경우 종료
-                print("2 아웃!! ㅠㅠ")
-                print("아까비~~~졌네용..")
-                print("정답은 : ",gameList,"입니다")
-                break
         if strike > 0:
             print(strike," Strike",end=" ")
         if ball > 0:
