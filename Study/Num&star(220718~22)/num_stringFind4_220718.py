ac7ed8355019b90bcbe608e16e03bbbf792b7fca
Lainari <108247620+Lainari@users.noreply.github.com>
2022-07-18 20:20:06 +0900

    Update num_stringFind4_220718.py

--- Study/Num&star(220718~22)/num_stringFind4_220718.py
+++ Study/Num&star(220718~22)/num_stringFind4_220718.py
@@ -13,30 +13,43 @@ for index in range(lineNum):
 while True:
     findList=input("찾고 싶은 문자열을 입력해주세요 : ")
     for line in range(lineNum):                     # 줄 생성
+        findCheck = 0
         preWord=""                                  # 앞 단어 변수
         nextWord=""                                 # 뒤 단어 변수
         indexNum = 0                                # 매칭 변수
         for word in range(len(textList[line])):     # 단어 생성
-            if word == len(textList[lineNum])-1:    # 뒤 단어 변수 값 지정
+            if word == (len(textList[line])-1):    # 뒤 단어 변수 값 지정
                 nextWord = ""
             else:
                 nextWord = textList[line][word+1]
+
             # 매칭 시작
             if textList[line][word] == findList[indexNum] and indexNum==0:
                 indexNum += 1
             # 매칭 중
             elif textList[line][word] == findList[indexNum]:
+
                 # 매칭 완료
-                if indexNum == len(findList) -1:
+                if indexNum == (len(findList) -1) and (preWord==findList[indexNum-2] and (nextWord==" " or nextWord=="")):
                     findCount += 1
-                    findLine.append(line+1)
+                    findCheck += 1
                     indexNum = 0
+
+                elif indexNum == (len(findList)-1):
+                    indexNum = 0
+
                 # 매칭 계속
                 else:
                     indexNum += 1
+
             # 매칭 실패
             else:
                 indexNum = 0
+            preWord=textList[line][word]
+
+        if findCheck >= 1:
+            findLine.append(line+1)
+
     if findCount >= 1:
         print(findList,"를 찾았습니다.")
         print("검색된 라인 : ",findLine)
