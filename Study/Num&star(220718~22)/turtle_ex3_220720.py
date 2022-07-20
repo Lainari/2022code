8b57daf55f4b6c33aea97db1d901b7f100165fc7
Lainari <108247620+Lainari@users.noreply.github.com>
2022-07-20 11:29:39 +0900

    Create turtle_ex3_220720.py

---
+++ Study/Num&star(220718~22)/turtle_ex3_220720.py
@@ -0,0 +1,10 @@
+import turtle
+
+myTurtle=turtle.Turtle()
+
+myTurtle.screen.bgcolor("Black") # 배경색 변경(검정)
+myTurtle.color("BLUE") # 펜 색상 블루
+myTurtle.width(12) # 펜 굵기 12
+myTurtle.circle(50) # 현 위치 기준 반지름 50픽셀 원 출력
+
+turtle.done()
